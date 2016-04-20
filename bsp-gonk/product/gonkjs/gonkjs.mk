d9df5cbda1e3f0b2eba4e9fa819c956aaf587853
Michael Vines <mvines@silklabs.com>
2016-04-20 14:47:06 -0700

    Add gonkjs product

---
+++ bsp-gonk/product/gonkjs/gonkjs.mk
@@ -0,0 +1,12 @@
+PRODUCT_PACKAGES += node
+
+# vendor/silk/stubs/
+PRODUCT_PACKAGES += fakeappops gonksched
+
+# vendor/silk/init/
+PRODUCT_PACKAGES += \
+  init.silk.rc \
+  silk-kmsg \
+  silk-init \
+  silk-init-js \
+  silk-debug-wrapper \

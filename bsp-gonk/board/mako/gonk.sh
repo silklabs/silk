78652c2c80ffa51e1551b7a4dee6d5ea26125f1a
Michael Vines <mvines@silklabs.com>
2016-04-20 16:22:37 -0700

    Add mako board

---
+++ bsp-gonk/board/mako/gonk.sh
@@ -0,0 +1,11 @@
+# Copyright (c) 2015-2016 Silk Labs, Inc.
+# All Rights Reserved.
+# Confidential and Proprietary - Silk Labs, Inc.
+#
+# Sets up environment needed to build this gonk
+#
+
+export TARGET_PRODUCT=aosp_mako
+export TARGET_BUILD_VARIANT=eng
+export TARGET_BUILD_TYPE=release
+

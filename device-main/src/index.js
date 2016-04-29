252a5760635ee1c9296c42fb448f5fa9af90b992
James Lal <james@silklabs.com>
2016-04-29 15:23:46 -0700

    Unbreak sos device-main getprop only accepts one argument

--- device-main/src/index.js
+++ device-main/src/index.js
@@ -4,7 +4,7 @@
 const log = require('silk-alog');
 const properties = require('silk-properties');
 
-const productName = properties.get('ro.product.name', '(unknown?)');
+const productName = properties.get('ro.product.name') || '(unknown?)';
 
 log.info('Running on a', productName);
 setInterval(() => log.verbose('Hello world'), 1000);

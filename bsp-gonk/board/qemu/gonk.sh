6a16bfd075b19ef6ce6ccf19cb6a7199af7df557
Michael Vines <mvines@silklabs.com>
2016-04-29 17:15:15 -0700

    Fix license

--- bsp-gonk/board/qemu/gonk.sh
+++ bsp-gonk/board/qemu/gonk.sh
@@ -1,11 +1,3 @@
-# Copyright (c) 2015-2016 Silk Labs, Inc.
-# All Rights Reserved.
-# Confidential and Proprietary - Silk Labs, Inc.
-#
-# Sets up environment needed to build this gonk
-#
-
 export TARGET_PRODUCT=aosp_arm
 export TARGET_BUILD_VARIANT=eng
 export TARGET_BUILD_TYPE=release
-

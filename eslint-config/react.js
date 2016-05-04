d8527f6f1994040038c03f2e082e171235869a59
Philipp von Weitershausen <philikon@silklabs.com>
2016-05-04 16:01:38 -0700

    Import React API from 'react' and React Native API from 'react-native'

--- eslint-config/react.js
+++ eslint-config/react.js
@@ -7,7 +7,10 @@ module.exports = {
     "react"
   ],
   "rules": {
+    "react/jsx-closing-bracket-location": [2, {"nonEmpty": "after-props", "selfClosing": "tag-aligned"}],
     "react/jsx-indent-props": [2, 2],
-    "react/jsx-closing-bracket-location": [2, {"nonEmpty": "after-props", "selfClosing": "tag-aligned"}]
+    'react/jsx-no-undef': 2,
+    'react/jsx-uses-react': 2,
+    'react/react-in-jsx-scope': 2
   }
 };

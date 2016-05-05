0dbc1ccb21fa8eb2547506dec27212c4bf7d91d1
James Lal <james@silklabs.com>
2016-05-04 17:57:41 -0700

    introduce persist.silk.main to control which JS module is main

--- bsp-gonk/vendor/silk/init/silk-init.js
+++ bsp-gonk/vendor/silk/init/silk-init.js
@@ -1,1 +1,8 @@
-require('silk-device-main');
+'use strict';
+
+const props = require('silk-properties');
+const MAIN_PROP = 'persist.silk.main';
+
+const main = props.get(MAIN_PROP) || 'silk-device-main';
+console.log(`initializing silk with main: ${main}`);
+require(main);

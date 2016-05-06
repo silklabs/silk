13e72d86c280e26f45b0ded95b17d678392490ce
James Lal <james@silklabs.com>
2016-05-05 23:17:27 -0700

    log: Remove babel5 usage

--- log/test/setup.js
+++ log/test/setup.js
@@ -1,9 +1,8 @@
 // XXX: Hack to enable android log formatting for tests.
 process.env.SILK_ANDROID_LOGS = 1;
-require('babel/register')(
-  {
-    "breakConfig": true,
-    "stage": "1",
-    "optional": ["regenerator", "asyncToGenerator", "es7.classProperties"]
-  }
-);
+
+const preset = require('../../babel-preset-silk-node4');
+
+require('../../babel/register')({
+  presets: [preset]
+});

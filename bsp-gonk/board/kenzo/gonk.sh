1fe67d6c1f6feed50c0fc12e66a9519762cb78cb
Michael Vines <mvines@silklabs.com>
2016-05-05 20:47:19 -0700

    Reduce size of kenzo build

--- bsp-gonk/board/kenzo/gonk.sh
+++ bsp-gonk/board/kenzo/gonk.sh
@@ -1,5 +1,5 @@
 # Sets up shell environment needed to build this board
 export TARGET_PRODUCT=msm8952_64
-export TARGET_BUILD_VARIANT=eng
+export TARGET_BUILD_VARIANT=userdebug
 export TARGET_BUILD_TYPE=release
 

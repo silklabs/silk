88bdd83fbabe53685623d936a21212efe0b7efad
Anshul Jain <anshul@silklabs.com>
2016-05-12 00:27:52 -0700

    Update silk-sdk documentation

--- sdk/interface/silk-alog.js
+++ sdk/interface/silk-alog.js
@@ -1,9 +1,11 @@
 /**
  * silk-alog module interface
+ *
+ * @private
  * @flow
  */
 
-type SilkALogFunc = (tagOrMessage: string, maybeMessage?: string) => void;
+type SilkALogFunc = (tagOrMessage: string, maybeMessage: ?string) => void;
 
 declare module "silk-alog" {
   declare var verbose: SilkALogFunc;
@@ -13,3 +15,78 @@ declare module "silk-alog" {
   declare var error: SilkALogFunc;
   declare var fatal: SilkALogFunc;
 }
+
+/**
+ * Silk logging helper
+ *
+ * @module silk-alog
+ * @example
+ * import log from 'silk-alog';
+ *
+ * log.verbose('tag', 'hello there');
+ * log.debug('default log tag is node');
+ * log.info('hi');
+ * log.warn('tsktsk');
+ * log.error('ono');
+ * log.fatal('sos');
+ */
+
+/**
+ * Print verbose log message
+ *
+ * @name verbose
+ * @instance
+ * @memberof silk-alog
+ * @param {string} [tag='node'] Source of the log message
+ * @param {string} message The message to log
+ */
+
+/**
+ * Print debug log message
+ *
+ * @name debug
+ * @instance
+ * @memberof silk-alog
+ * @param {string} [tag='node'] Source of the log message
+ * @param {string} message The message to log
+ */
+
+/**
+ * Print info log message
+ *
+ * @name info
+ * @instance
+ * @memberof silk-alog
+ * @param {string} [tag='node'] Source of the log message
+ * @param {string} message The message to log
+ */
+
+/**
+ * Print warn log message
+ *
+ * @name warn
+ * @instance
+ * @memberof silk-alog
+ * @param {string} [tag='node'] Source of the log message
+ * @param {string} message The message to log
+ */
+
+/**
+ * Print error log message
+ *
+ * @name error
+ * @instance
+ * @memberof silk-alog
+ * @param {string} [tag='node'] Source of the log message
+ * @param {string} message The message to log
+ */
+
+/**
+ * Print fatal log message
+ *
+ * @name fatal
+ * @instance
+ * @memberof silk-alog
+ * @param {string} [tag='node'] Source of the log message
+ * @param {string} message The message to log
+ */
--- sdk/interface/silk-properties.js
+++ sdk/interface/silk-properties.js
@@ -1,5 +1,6 @@
 /**
  * silk-properties
+ * @private
  * @flow
  */
 
@@ -7,3 +8,23 @@ declare module "silk-properties" {
   declare function get(name: string): string;
 }
 
+/**
+ * Silk system properties helper
+ *
+ * @module silk-properties
+ * @example
+ * import props from 'silk-properties';
+ *
+ * let value = props.get('persist.silk.main');
+ * log.info('Value: ' + value);
+ */
+
+/**
+ * Get system property
+ *
+ * @name get
+ * @instance
+ * @memberof silk-properties
+ * @param {string} key The name of the system property to fetch
+ * @return {string} Returns the value of a system property or null if no such property exists
+ */

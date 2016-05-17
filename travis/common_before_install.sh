fbcc8ee6c3f2fc76ba562cfd77c1017672fc758b
Michael Vines <mvines@silklabs.com>
2016-05-17 13:40:08 -0700

    Reduce travis_{linux,darwin}.yml duplication

---
+++ travis/common_before_install.sh
@@ -0,0 +1,26 @@
+#
+# Travis before_install things that most builds should run
+#
+
+git --version
+git config --global user.email "stooge@silklabs.com"
+git config --global user.name "Stooge"
+git config --global color.ui false
+if [ $TRAVIS_OS_NAME == osx ]; then
+  echo HOME: $HOME # log.debug
+  echo PWD: $PWD   # log.debug
+
+  mkdir $HOME/.nvm
+  export NVM_DIR="$HOME/.nvm"
+
+  # Update brew twice because the first run can fail: https://github.com/Homebrew/homebrew/issues/42553
+  brew update; brew update
+  brew reinstall cmake coreutils libtool nvm xz
+
+  source $(brew --prefix nvm)/nvm.sh
+  npm config set spin=false
+fi
+nvm install 4.2
+nvm use 4.2
+node --version
+npm --version

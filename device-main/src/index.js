d4a413cd6e76ba063c079d799fd2efc2b475e210
Anshul Jain <anshul@silklabs.com>
2016-05-18 01:11:06 -0700

    Shutdown the device if running out of battery closes #1553

--- device-main/src/index.js
+++ device-main/src/index.js
@@ -4,21 +4,37 @@
 const log = require('silk-alog');
 const wifi = require('silk-wifi').default;
 const util = require('silk-sysutils');
+const Battery = require('silk-battery').default;
 
 const productName = util.getstrprop('ro.product.name', '(unknown?)');
 
 log.info('Running on a ' + productName);
 
+// Shutdown the device if running out of power so as to not completely drain
+// the battery
+const battery = new Battery();
+battery.init()
+.then(() => {
+  battery.on('out-of-power', shutdown);
+});
+
 log.info('Initializing wifi');
 wifi.init()
-.then(function() {
+.then(() => {
   return wifi.online();
 })
-.then(function() {
+.then(() => {
   log.info('Wifi initialized successfully');
   setInterval(() => log.verbose('Hello world'), 1000);
 })
-.catch(function(err) {
+.catch((err) => {
   log.error('Failed to initialize', err.stack || err);
 });
 
+/**
+ * Shutdown the device
+ */
+function shutdown() {
+  log.warn('Device ran out of power; shutting down...');
+  util.setprop('sys.powerctl', 'shutdown');
+}

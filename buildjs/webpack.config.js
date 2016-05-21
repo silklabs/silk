ec4e8d78f53c455de4cbfa2f69e8d846c4e5dcc7
James Lal <james@silklabs.com>
2016-05-21 13:38:30 -0700

    buildjs: Upgrade to allow building native modules

--- buildjs/webpack.config.js
+++ buildjs/webpack.config.js
@@ -6,33 +6,132 @@ const fs = require('fs');
 const findPackage = require('./src/find_package.js');
 
 const webpack = require('webpack');
-const CopyWebpack = require('copy-webpack-plugin');
+const resolve = require('resolve');
+const mkdirp = require('mkdirp');
 
 // We must place our files in a special folder for integration /w the android
 // build system.
-const destination = `.silkslug`;
-const context = findPackage();
-const pkg = require(path.join(context, 'package.json'));
-const localWebpack = path.join(context, 'webpack.config.js');
+const destination = process.env.SILK_BUILDJS_DEST;
+const babelCache = path.join(destination, '../.babelcache');
+const modulePath = findPackage(process.env.SILK_BUILDJS_SOURCE);
+const pkg = require(path.join(modulePath, 'package.json'));
+const localWebpack = path.join(modulePath, 'webpack.config.js');
+
+mkdirp.sync(babelCache);
 
 const name = pkg.name;
-const main = pkg.main;
+let main = pkg.main || './index.js';
+if (!path.extname(main)) {
+  main += '.js';
+}
+
+if (main.indexOf('./') !== 0) {
+  main = `./${main}`;
+}
+
+// XXX: Would be nicer to abort this in the bash script rather than after we
+// have booted up node here...
+if (
+  main.indexOf('build/Release') === 0 ||
+  main.indexOf('./build/Release') === 0 ||
+  /\.node$/.test(main)
+) {
+  console.log(`Module contains no JS main skipping webpack ...`);
+  process.exit(0);
+}
 
 if (!main) {
   throw new Error(`package.json must have main in ${process.cwd()}`);
 }
 
+const externals = [
+  // TODO: auto generate these ...
+  'base_version',
+  'bleno',
+  'mic',
+  'noble',
+  'node-webrtc',
+  'opencv',
+  'segfault-handler',
+  'silk-alog',
+  'silk-battery',
+  'silk-camera',
+  'silk-core-version',
+  'silk-cv',
+  'silk-gc1',
+  'silk-input',
+  'silk-lights',
+  'silk-movie',
+  'silk-ntp',
+  'silk-properties',
+  'silk-sensors',
+  'silk-sysutils',
+  'silk-vibrator',
+  'silk-wifi',
+  'v8-profiler',
+  /\.node$/,
+  (context, request, callback) => {
+    if (resolve.isCore(request)) {
+      callback(null, true);
+      return;
+    }
+
+    // For extra fun node will allow resolving .main without a ./ this behavior
+    // makes .main look nicer but is completely different than how requiring
+    // without a specific path is handled elsewhere... To ensure we don't
+    // accidentally resolve a node module to a local file we handle this case
+    // very specifically.
+    if (context === modulePath && pkg.main === request) {
+      const resolvedPath = path.resolve(context, request);
+      if (!fs.existsSync(resolvedPath)) {
+        callback(new Error(`${modulePath} has a .main which is missing ...`));
+        return;
+      }
+    }
+
+    resolve(request,  {
+      basedir: context,
+      package: pkg,
+      extensions: ['.js', '.json'],
+    }, (err, resolvedPath) => {
+      if (err) {
+        console.log(`Missing module imported from ${context} (${request})`);
+        callback(null, true);
+        return;
+      }
+      callback(null, false);
+    });
+  }
+];
+
+const entry = {};
+entry[main] = main;
+
 const config = {
-  context,
+  context: modulePath,
+  externals,
   target: 'node',
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
   devtool: 'source-map',
-  entry: `./${main}`,
+  entry,
   output: {
-    path: path.join(context, destination),
+    path: path.join(destination),
     libraryTarget: 'commonjs2',
-    filename: `./${main}`,
+    filename: `[name]`,
+  },
+  resolve: {
+    extensions: ['', '.js', '.json'],
+    // Common "gothcha" modules ...
+    alias: {
+      'any-promise': require.resolve('./shims/any-promise.js'),
+      'json3': require.resolve('./shims/json3.js'),
+    },
   },
   babel: {
+    cacheDirectory: babelCache,
     presets: [
       require('babel-preset-silk-node4'),
     ],
@@ -48,32 +147,19 @@ const config = {
         }
       }
     ]
-  },
-  plugins: [new CopyWebpack([
-    { from: 'package.json' }
-  ])],
+  }
 }
 
 if (fs.existsSync(localWebpack)) {
   console.log(`${localWebpack} found agumenting buildjs ...`);
-  Object.assign(config, require(localWebpack));
-}
-
-// Create symlink into package root dir for simulator to work
-try {
-  fs.symlinkSync(path.join(destination, main), path.join(context, main));
-} catch(err) {
-  if (err.code !== 'EEXIST') {
-    console.log(err);
+  const rules = Object.assign({}, require(localWebpack));
+  if (rules.externals && Array.isArray(rules.externals)) {
+    // Order is important here the rules should be ordered such that the
+    // webpack.config comes from the module first and our global rules second.
+    config.externals = rules.externals.concat(config.externals);
+    delete rules.externals;
   }
-}
-
-try {
-  fs.symlinkSync(path.join(destination, `${main}.map`), path.join(context, `${main}.map`));
-} catch(err) {
-  if (err.code !== 'EEXIST') {
-    console.log(err);
-  }
+  Object.assign(config, rules);
 }
 
 module.exports = config;

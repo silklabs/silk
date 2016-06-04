e6e6731fbb10c4ec90e265fac20504450e7d4ef5
Michael Vines <mvines@silklabs.com>
2016-06-03 17:34:43 -0700

    Emit silk.module.push system prop once a module push completes

--- sdk/src/api.js
+++ sdk/src/api.js
@@ -12,6 +12,7 @@ import fs from 'mz/fs';
 const SILK_MODULE_ROOT = '/system/silk/node_modules';
 const DATA_MODULE_ROOT = '/data/node_modules';
 const ACTIVATE_PROP = 'persist.silk.main';
+const MODULE_PUSH_PROP = 'silk.module.push';
 const WIFI_SETUP_SCRIPT = 'wifi_setup.sh';
 
 async function execWithPaths(
@@ -109,6 +110,7 @@ export default class API {
     console.log('Updating', dest);
     await this.adb(`shell rm -rf ${dest}`);
     await this.adb(`push ${directory} ${dest}`, /*timeout = */ 60 * 1000);
+    this.setprop(MODULE_PUSH_PROP, dest);
   }
 
   async setprop(key, value) {

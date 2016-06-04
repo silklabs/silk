1f28d8570013df977a75ce78f0fccef515f9237e
Ahmed Nefzaoui <nefzaoui.ahmed@hotmail.com>
2016-06-04 19:26:55 +0100

    Power Management in Silk template

--- cli/templates/init/device.js
+++ cli/templates/init/device.js
@@ -5,6 +5,7 @@ const Vibrator = require('silk-vibrator').default;
 const wifi = require('silk-wifi').default;
 const Input = require('silk-input').default;
 const util = require('silk-sysutils');
+const Battery = require('silk-battery').default;
 
 function bail(err) {
   log.error(err.stack || err);
@@ -16,19 +17,30 @@ process.on('uncaughtException', bail);
 module.exports = {
   init: () => {
     /**
+     * Shutdown the device if running out of power so as to not completely drain
+     * the battery
+     */
+    const battery = new Battery();
+    battery.init();
+    battery.on('out-of-power', () => {
+      log.warn('Device ran out of power; shutting down...');
+      shutdown();
+    });
+
+    /**
      * Initializing the Wi-Fi module.
      */
 
     wifi.init()
-      .then(() => {
-        return wifi.online();
-      })
-      .then(() => {
-        log.info('Wifi initialized successfully');
-      })
-      .catch((err) => {
-        log.error('Failed to initialize wifi', err);
-      });
+    .then(() => {
+      return wifi.online();
+    })
+    .then(() => {
+      log.info('Wifi initialized successfully');
+    })
+    .catch((err) => {
+      log.error('Failed to initialize wifi', err);
+    });
 
     // Power key handling
     let input = new Input();

2543aa2d60302361126aa9bbe551f3c52735b02c
James Lal <james@silklabs.com>
2016-07-13 18:02:10 -0700

    log: Remove use of babel-register

--- log/test/setup.js
+++ log/test/setup.js
@@ -1,8 +1,2 @@
 // XXX: Hack to enable android log formatting for tests.
 process.env.SILK_ANDROID_LOGS = 1;
-
-const preset = require('../../babel-preset-silk-node4');
-
-require('../../babel-register')({
-  presets: [preset]
-});

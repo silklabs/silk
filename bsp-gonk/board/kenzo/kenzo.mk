12aaa9ca84c8246a74e7589b53a90315d96d3033
Anshul Jain <anshul@silklabs.com>
2016-09-21 09:35:55 -0700

    Create fake OpenCL for full kenzo builds
    Kenzo build doesn't seem to have source code to compile opencl
    so fake it to make node-opencl happy

--- bsp-gonk/board/kenzo/kenzo.mk
+++ bsp-gonk/board/kenzo/kenzo.mk
@@ -1,6 +1,5 @@
 # Sets up make product needed to build this board
 TARGET_QBLUETOOTH_HCI_CMD_SEND=true
-TARGET_NO_FAKE_LIBOPENCL=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
  ro.silk.camera.focus_mode=auto

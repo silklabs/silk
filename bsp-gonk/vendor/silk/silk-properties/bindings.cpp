c56f0b3c5d679321f2861ffeb5b7f23fe7ec017b
Michael Vines <mvines@silklabs.com>
2016-11-09 11:21:48 -0800

    Add reference getprop/setprop implementation

--- bsp-gonk/vendor/silk/silk-properties/bindings.cpp
+++ bsp-gonk/vendor/silk/silk-properties/bindings.cpp
@@ -12,17 +12,36 @@ NAN_METHOD(GetProperty) {
     return Nan::ThrowError("Expected only one string argument: GetProperty");
   }
 
+  char *property_name = *Nan::Utf8String(info[0]);
   char property_value[PROPERTY_VALUE_MAX];
   const char* default_value = "";
   int len;
 #ifdef ANDROID
-  char *property_name = *Nan::Utf8String(info[0]);
   len = property_get(property_name, property_value, default_value);
 #else
-  // Return default value
-  strncpy(property_value, default_value, sizeof(property_value) - 1);
+  char file[PROPERTY_VALUE_MAX * 2];
+  snprintf(file, sizeof(file), "data/properties/%s", property_name);
+  FILE *fp = fopen(file, "r");
+  if (fp != NULL) {
+    fgets(property_value, sizeof(property_value) - 1, fp);
+    fclose(fp);
+  } else {
+    // Return default value
+    strncpy(property_value, default_value, sizeof(property_value) - 1);
+  }
   property_value[sizeof(property_value)-1] = '\0';
   len = strlen(property_value);
+
+  // Remove trailing newline
+  while (len > 0) {
+    char c = property_value[len - 1];
+    if (c == '\n' || c == '\r') {
+      property_value[len - 1] = '\0';
+      len--;
+    } else {
+      break;
+    }
+  }
 #endif
 
   info.GetReturnValue().Set(
@@ -30,7 +49,6 @@ NAN_METHOD(GetProperty) {
   );
 }
 
-#ifdef ANDROID
 NAN_METHOD(SetProperty) {
   Nan::HandleScope scope;
 
@@ -40,19 +58,30 @@ NAN_METHOD(SetProperty) {
 
   v8::String::Utf8Value property_name(info[0]);
   v8::String::Utf8Value property_value(info[1]);
+#ifdef ANDROID
   int result = property_set(*property_name, *property_value);
   info.GetReturnValue().Set(Nan::New<v8::Number>(result));
-}
+#else
+  int result = -1;
+  char file[PROPERTY_VALUE_MAX * 2];
+  snprintf(file, sizeof(file), "data/properties/%s", *property_name);
+  FILE *fp = fopen(file, "w");
+  if (fp != NULL) {
+    fputs(*property_value, fp);
+    fputs("\n", fp);
+    fclose(fp);
+    result = 0;
+  }
+  info.GetReturnValue().Set(Nan::New<v8::Number>(result));
 #endif
+}
 
 void init(v8::Handle<v8::Object> exports)
 {
   exports->Set(Nan::New<v8::String>("get").ToLocalChecked(),
     Nan::New<v8::FunctionTemplate>(GetProperty)->GetFunction());
-#ifdef ANDROID
   exports->Set(Nan::New<v8::String>("set").ToLocalChecked(),
     Nan::New<v8::FunctionTemplate>(SetProperty)->GetFunction());
-#endif
 }
 
 NODE_MODULE(properties, init)

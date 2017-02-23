9bee085cbb487d3f440a0ad936ef5e7579c88c0a
Michael Vines <mvines@silklabs.com>
2017-02-22 19:19:35 -0800

    Update node_modules

---
+++ node_modules/opencv/build/Release/.deps/Release/opencv.node.d
@@ -0,0 +1,1 @@
+cmd_Release/opencv.node := rm -rf "Release/opencv.node" && cp -af "Release/obj.target/opencv.node" "Release/opencv.node"
---
+++ node_modules/opencv/build/Release/.deps/Release/test_nativemat.node.d
@@ -0,0 +1,1 @@
+cmd_Release/test_nativemat.node := rm -rf "Release/test_nativemat.node" && cp -af "Release/obj.target/test_nativemat.node" "Release/test_nativemat.node"

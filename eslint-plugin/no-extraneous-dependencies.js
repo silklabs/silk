2ec644d4ab7d099696ea543d910927b4b0df0bc6
Philipp von Weitershausen <philikon@silklabs.com>
2017-03-02 00:50:05 -0800

    eslint-plugin-silk: fix imports and add semicolons to no-extraneous-dependencies rule

--- eslint-plugin/no-extraneous-dependencies.js
+++ eslint-plugin/no-extraneous-dependencies.js
@@ -1,85 +1,91 @@
-import path from 'path'
-import readPkgUp from 'read-pkg-up'
-import minimatch from 'minimatch'
-import importType from '../core/importType'
-import isStaticRequire from '../core/staticRequire'
+/**
+ * eslint rule that ensures every require()'d and imported package is declared
+ * in package.json. It is based on import/no-extraneous-dependencies.
+ */
+
+const path = require('path');
+const readPkgUp = require('read-pkg-up');
+const minimatch = require('minimatch');
+
+const importType = require('eslint-plugin-import/lib/core/importType').default;
+const isStaticRequire = require('eslint-plugin-import/lib/core/staticRequire').default;
 
 function getDependencies(context) {
   try {
-    const pkg = readPkgUp.sync({cwd: context.getFilename(), normalize: false})
+    const pkg = readPkgUp.sync({cwd: context.getFilename(), normalize: false});
     if (!pkg || !pkg.pkg) {
-      return null
+      return null;
     }
-    const packageContent = pkg.pkg
+    const packageContent = pkg.pkg;
     return {
       dependencies: packageContent.dependencies || {},
       devDependencies: packageContent.devDependencies || {},
       optionalDependencies: packageContent.optionalDependencies || {},
       peerDependencies: packageContent.peerDependencies || {},
-    }
+    };
   } catch (e) {
-    return null
+    return null;
   }
 }
 
 function missingErrorMessage(packageName) {
   return `'${packageName}' should be listed in the project's dependencies. ` +
-    `Run 'npm i -S ${packageName}' to add it`
+    `Run 'npm i -S ${packageName}' to add it`;
 }
 
 function devDepErrorMessage(packageName) {
-  return `'${packageName}' should be listed in the project's dependencies, not devDependencies.`
+  return `'${packageName}' should be listed in the project's dependencies, not devDependencies.`;
 }
 
 function optDepErrorMessage(packageName) {
   return `'${packageName}' should be listed in the project's dependencies, ` +
-    `not optionalDependencies.`
+    `not optionalDependencies.`;
 }
 
 function reportIfMissing(context, deps, depsOptions, node, name) {
   if (importType(name, context) !== 'external') {
-    return
+    return;
   }
-  const splitName = name.split('/')
+  const splitName = name.split('/');
   const packageName = splitName[0][0] === '@'
     ? splitName.slice(0, 2).join('/')
-    : splitName[0]
-  const isInDeps = deps.dependencies[packageName] !== undefined
-  const isInDevDeps = deps.devDependencies[packageName] !== undefined
-  const isInOptDeps = deps.optionalDependencies[packageName] !== undefined
-  const isInPeerDeps = deps.peerDependencies[packageName] !== undefined
+    : splitName[0];
+  const isInDeps = deps.dependencies[packageName] !== undefined;
+  const isInDevDeps = deps.devDependencies[packageName] !== undefined;
+  const isInOptDeps = deps.optionalDependencies[packageName] !== undefined;
+  const isInPeerDeps = deps.peerDependencies[packageName] !== undefined;
 
   if (isInDeps ||
     (depsOptions.allowDevDeps && isInDevDeps) ||
     (depsOptions.allowPeerDeps && isInPeerDeps) ||
     (depsOptions.allowOptDeps && isInOptDeps)
   ) {
-    return
+    return;
   }
 
   if (isInDevDeps && !depsOptions.allowDevDeps) {
-    context.report(node, devDepErrorMessage(packageName))
-    return
+    context.report(node, devDepErrorMessage(packageName));
+    return;
   }
 
   if (isInOptDeps && !depsOptions.allowOptDeps) {
-    context.report(node, optDepErrorMessage(packageName))
-    return
+    context.report(node, optDepErrorMessage(packageName));
+    return;
   }
 
-  context.report(node, missingErrorMessage(packageName))
+  context.report(node, missingErrorMessage(packageName));
 }
 
 function testConfig(config, filename) {
   // Simplest configuration first, either a boolean or nothing.
   if (typeof config === 'boolean' || typeof config === 'undefined') {
-    return config
+    return config;
   }
   // Array of globs.
   return config.some(c => (
     minimatch(filename, c) ||
     minimatch(filename, path.join(process.cwd(), c))
-  ))
+  ));
 }
 
 module.exports = {
@@ -99,31 +105,31 @@ module.exports = {
     ],
   },
 
-  create: function (context) {
-    const options = context.options[0] || {}
-    const filename = context.getFilename()
-    const deps = getDependencies(context)
+  create(context) {
+    const options = context.options[0] || {};
+    const filename = context.getFilename();
+    const deps = getDependencies(context);
 
     if (!deps) {
-      return {}
+      return {};
     }
 
     const depsOptions = {
       allowDevDeps: testConfig(options.devDependencies, filename) !== false,
       allowOptDeps: testConfig(options.optionalDependencies, filename) !== false,
       allowPeerDeps: testConfig(options.peerDependencies, filename) !== false,
-    }
+    };
 
     // todo: use module visitor from module-utils core
     return {
       ImportDeclaration: function (node) {
-        reportIfMissing(context, deps, depsOptions, node, node.source.value)
+        reportIfMissing(context, deps, depsOptions, node, node.source.value);
       },
       CallExpression: function handleRequires(node) {
         if (isStaticRequire(node)) {
-          reportIfMissing(context, deps, depsOptions, node, node.arguments[0].value)
+          reportIfMissing(context, deps, depsOptions, node, node.arguments[0].value);
         }
       },
-    }
+    };
   },
-}
+};

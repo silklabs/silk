28b75b2ebb2ec3a5c3d04137ad0ec00633f0467c
Anshul Jain <anshul@silklabs.com>
2017-04-13 10:36:44 -0700

    Don't install tools in the production environment

--- bsp-gonk/vendor/silk/silk-audioplayer/webpack.config.js
+++ bsp-gonk/vendor/silk/silk-audioplayer/webpack.config.js
@@ -1,17 +1,26 @@
 /* eslint-disable flowtype/require-valid-file-annotation */
 const CopyWebpack = require('copy-webpack-plugin');
 
-module.exports = {
-  entry: {
-    'lib/index.js': './lib/index.js',
-    'tools/play_audiofile.js': './tools/play_audiofile.js',
-    'tools/play_filestream.js': './tools/play_filestream.js',
-    'tools/play_shoutcast.js': './tools/play_shoutcast.js',
-    'tools/play_progressive.js': './tools/play_progressive.js',
-  },
-  plugins: [
-    new CopyWebpack([
-      {from: 'media', to: 'media'},
-    ]),
-  ],
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    entry: {
+      'lib/index.js': './lib/index.js',
+    },
+  };
+} else {
+  // Only install tools in development builds
+  module.exports = {
+    entry: {
+      'lib/index.js': './lib/index.js',
+      'tools/play_audiofile.js': './tools/play_audiofile.js',
+      'tools/play_filestream.js': './tools/play_filestream.js',
+      'tools/play_shoutcast.js': './tools/play_shoutcast.js',
+      'tools/play_progressive.js': './tools/play_progressive.js',
+    },
+    plugins: [
+      new CopyWebpack([
+        {from: 'media', to: 'media'},
+      ]),
+    ],
+  };
+}

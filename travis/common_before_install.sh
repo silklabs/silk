7e639065792d7daf3f3189e1432c665a5f754b08
Michael Vines <mvines@silklabs.com>
2017-04-16 22:27:21 -0700

    Install ninja

--- travis/common_before_install.sh
+++ travis/common_before_install.sh
@@ -6,21 +6,31 @@ git --version
 git config --global user.email "stooge@silklabs.com"
 git config --global user.name "Stooge"
 git config --global color.ui false
-if [ -n $TRAVIS ] && [ $CI_OS_NAME == osx ]; then
-  # Travis OS X machines need some more installing.
-  echo HOME: $HOME # log.debug
-  echo PWD: $PWD   # log.debug
+if [ -n ${TRAVIS:-false} ]; then
+  if [ $CI_OS_NAME == linux ]; then
+    # Install ninja
+    wget -q https://github.com/ninja-build/ninja/releases/download/v1.7.2/ninja-linux.zip
+    unzip ninja-linux.zip
+    rm ninja-linux.zip
+    sudo mv ninja /usr/bin
+  fi
+  if [ $CI_OS_NAME == osx ]; then
+    # Travis OS X machines need some more installing.
+    echo HOME: $HOME # log.debug
+    echo PWD: $PWD   # log.debug
 
-  mkdir $HOME/.nvm
-  export NVM_DIR="$HOME/.nvm"
+    mkdir $HOME/.nvm
+    export NVM_DIR="$HOME/.nvm"
 
-  # Update brew twice because the first run can fail: https://github.com/Homebrew/homebrew/issues/42553
-  brew update; brew update
-  brew reinstall cmake coreutils libtool xz
-  brew install nvm
+    # Update brew twice because the first run can fail: https://github.com/Homebrew/homebrew/issues/42553
+    brew update; brew update
+    brew reinstall cmake coreutils libtool xz
+    brew install ninja
+    brew install nvm
 
-  source $(brew --prefix nvm)/nvm.sh
-  npm config set spin=false
+    source $(brew --prefix nvm)/nvm.sh
+    npm config set spin=false
+  fi
 fi
 
 NODE_VERSION=6.9.1

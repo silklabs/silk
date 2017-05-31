1f24a72b5db58e73a13f88374e94102af92e33a5
Anshul Jain <anshul@silklabs.com>
2017-05-31 14:18:41 -0700

    Update play shoutcast to stress test audio-player

--- bsp-gonk/vendor/silk/silk-audioplayer/tools/play_shoutcast.js
+++ bsp-gonk/vendor/silk/silk-audioplayer/tools/play_shoutcast.js
@@ -5,53 +5,85 @@
 import Player from 'silk-audioplayer';
 import icy from 'icy';
 import createLog from 'silk-log';
+import {sleep, createDeferred} from 'silk-async-utils';
 const log = createLog('test');
 
 let url = 'http://kidspublicradio2.got.net:8000/lullaby?lang=en-US%2cen%3bq%3d0.8';
 
 let player = new Player();
+let shoutcastResponse = null;
+let deferred = null;
 
-// Connect to the remote stream
-icy.get(url, function (res) {
-
-  // Log the HTTP response headers
-  log.info(`headers: `, res.headers);
-
-  // Log any "metadata" events that happen
-  res.on('metadata', (metadata) => {
-    let parsed = icy.parse(metadata);
-    log.info(`parsed: `, parsed);
-  });
-
+function playShoutcast() {
   const onEnd = (error) => {
     if (error) {
       log.error(error.message);
     }
     log.info(`Shoutcast stream ended`);
+    if (deferred) {
+      deferred.accept();
+    }
+    if (player) {
+      player.removeAllListeners('error');
+      player.removeAllListeners('started');
+    }
   };
 
-  res.once('error', onEnd);
-  res.once('end', onEnd);
+  return new Promise((resolve, reject) => {
+    // Connect to the remote stream
+    icy.get(url, function (res) {
+      shoutcastResponse = res;
+      log.info(`headers: `, res.headers);
 
-  player.on('error', (err) => {
-    log.error(err);
-    process.exit(1); // eslint-disable-line
+      // Log any "metadata" events that happen
+      res.once('metadata', (metadata) => {
+        let parsed = icy.parse(metadata);
+        log.info(`parsed: `, parsed);
+      });
+
+      player.on('error', (err) => {
+        log.info('error');
+        log.error(err);
+        reject(err);
+      });
+      player.on('started', () => {
+        log.info(`Player started`);
+        resolve();
+      });
+
+      res.on('data', data => {
+        player.write(data);
+      });
+      res.once('error', onEnd);
+      res.once('end', onEnd);
+    });
   });
-  player.on('prepared', () => log.info(`Player prepared`));
-  player.on('paused', () => log.info(`Player paused`));
-  player.on('resumed', () => log.info(`Player resumed`));
-  player.on('started', () => {
-    log.info(`Player started`);
-    setTimeout(() => {
-      log.info(`Pausing now`);
-      player.pause();
-    }, 5000);
+}
 
-    setTimeout(() => {
-      log.info(`Resuming now`);
-      player.resume();
-    }, 7000);
+function stopShoutcast() {
+  deferred = createDeferred();
+  if (shoutcastResponse) {
+    log.info(`Stopping shoutcast`);
+    shoutcastResponse.res.client.destroy();
+    shoutcastResponse = null;
+  }
+
+  return deferred.promise
+  .then(() => {
+    deferred = null;
+    log.info(`Stopping audio player`);
+    return player.stop();
   });
+}
 
-  res.on('data', data => player.write(data));
-});
+async function test() {
+  for (;;) {
+    await playShoutcast();
+    await sleep(2000);
+    await stopShoutcast();
+  }
+}
+
+test()
+.then(() => log.info(`done`))
+.catch((err) => log.error(`Error: `, err));

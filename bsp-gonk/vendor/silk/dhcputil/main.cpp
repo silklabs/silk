214c4e85dfa7c24598d0075c836d764eb8a0e38e
Michael Vines <mvines@silklabs.com>
2017-09-04 15:13:40 -0700

    Set dhcp.%s.result system prop

--- bsp-gonk/vendor/silk/dhcputil/main.cpp
+++ bsp-gonk/vendor/silk/dhcputil/main.cpp
@@ -51,63 +51,65 @@ int main(int argc, char **argv)
 
   } else if (!strcmp(cmd, "dhcp_request")) {
 #ifdef TARGET_GE_NOUGAT
-    int err;
-    err = ifc_init();
+    char prop_name[PROPERTY_KEY_MAX];
+    char prop_value[PROPERTY_VALUE_MAX];
+
+    int err = ifc_init();
     if (err != 0) {
       printf("ifc_init() failed\n");
       return 1;
+    } else {
+      err = do_dhcp(ifname);
+      // Fill out the same system properties that older gonks set automatically
+      // when dhcp completes.
+      //
+      // Eventually it would be good to get away from using system properties to
+      // convey this information...
+      //
+      if (!err) {
+        uint32_t ipaddr;
+        uint32_t gateway;
+        uint32_t prefixLength;
+        uint32_t dns1;
+        uint32_t dns2;
+        uint32_t server;
+        uint32_t leasetime;
+        get_dhcp_info(
+          &ipaddr,
+          &gateway,
+          &prefixLength,
+          &dns1,
+          &dns2,
+          &server,
+          &leasetime
+        );
+
+        snprintf(prop_name, sizeof(prop_name), "dhcp.%s.ipaddress", ifname);
+        property_set(prop_name, ipaddr_to_string(ipaddr));
+
+        snprintf(prop_name, sizeof(prop_name), "dhcp.%s.mask", ifname);
+        snprintf(prop_value, sizeof(prop_value), "%d", prefixLength);
+        property_set(prop_name, prop_value);
+
+        snprintf(prop_name, sizeof(prop_name), "dhcp.%s.gateway", ifname);
+        property_set(prop_name, ipaddr_to_string(gateway));
+
+        snprintf(prop_name, sizeof(prop_name), "dhcp.%s.dns1", ifname);
+        property_set(prop_name, dns1 ? ipaddr_to_string(dns1) : "");
+
+        snprintf(prop_name, sizeof(prop_name), "dhcp.%s.dns2", ifname);
+        property_set(prop_name, dns2 ? ipaddr_to_string(dns2) : "");
+
+        snprintf(prop_name, sizeof(prop_name), "dhcp.%s.leasetime", ifname);
+        snprintf(prop_value, sizeof(prop_value), "%d", leasetime);
+        property_set(prop_name, prop_value);
+      }
+
+      ifc_close();
     }
+    snprintf(prop_name, sizeof(prop_name), "dhcp.%s.result", ifname);
+    property_set(prop_name, err ? "failure" : "ok");
 
-    err = do_dhcp(ifname);
-    // Fill out the same system properties that older gonks set automatically
-    // when dhcp completes.
-    //
-    // Eventually it would be good to get away from using system properties to
-    // convey this information...
-    //
-    if (!err) {
-      uint32_t ipaddr;
-      uint32_t gateway;
-      uint32_t prefixLength;
-      uint32_t dns1;
-      uint32_t dns2;
-      uint32_t server;
-      uint32_t leasetime;
-      get_dhcp_info(
-        &ipaddr,
-        &gateway,
-        &prefixLength,
-        &dns1,
-        &dns2,
-        &server,
-        &leasetime
-      );
-
-      char prop_name[PROPERTY_KEY_MAX];
-      char prop_value[PROPERTY_VALUE_MAX];
-
-      snprintf(prop_name, sizeof(prop_name), "dhcp.%s.ipaddress", ifname);
-      property_set(prop_name, ipaddr_to_string(ipaddr));
-
-      snprintf(prop_name, sizeof(prop_name), "dhcp.%s.mask", ifname);
-      snprintf(prop_value, sizeof(prop_value), "%d", prefixLength);
-      property_set(prop_name, prop_value);
-
-      snprintf(prop_name, sizeof(prop_name), "dhcp.%s.gateway", ifname);
-      property_set(prop_name, ipaddr_to_string(gateway));
-
-      snprintf(prop_name, sizeof(prop_name), "dhcp.%s.dns1", ifname);
-      property_set(prop_name, dns1 ? ipaddr_to_string(dns1) : "");
-
-      snprintf(prop_name, sizeof(prop_name), "dhcp.%s.dns2", ifname);
-      property_set(prop_name, dns2 ? ipaddr_to_string(dns2) : "");
-
-      snprintf(prop_name, sizeof(prop_name), "dhcp.%s.leasetime", ifname);
-      snprintf(prop_value, sizeof(prop_value), "%d", leasetime);
-      property_set(prop_name, prop_value);
-    }
-
-    ifc_close();
 #elif defined(TARGET_GE_MARSHMALLOW)
     int err = dhcp_start(ifname);
 #else

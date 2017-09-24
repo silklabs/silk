bd57ab819f35e5cab60a4ccae80198d3f52327a2
Michael Vines <mvines@silklabs.com>
2017-09-24 11:12:33 -0700

    Update paths are now configurable

--- bsp-gonk/vendor/silk/build/tasks/silkimage.mk
+++ bsp-gonk/vendor/silk/build/tasks/silkimage.mk
@@ -3,9 +3,10 @@
 # Confidential and Proprietary - Silk Labs, Inc.
 #
 # This task builds silk.img, which is an ext4 filesystem containing the contents
-# of /system/silk.  It may be pushed onto a device at /data/silk.img, and
-# activated via |adb shell setprop persist.silk.root silk.img|.  When active it
-# overrides the current contents of /system/silk.
+# of /system/silk.  It may be pushed onto a device at /data/silk.img (if not
+# overridden by ro.silk.update.img.file), and activated via |adb shell setprop
+# persist.silk.root silk.img|.  When active it overrides the current contents of
+# /system/silk.
 
 SILKIMAGE=$(PRODUCT_OUT)/silk.img
 .PHONY: $(SILKIMAGE)
